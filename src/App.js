976ed141d1eb4918d02b305ff7ce89176602158c
trung24012001 <hellovdtvip@gmail.com>
2021-02-14 00:52:34 +0700

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import Menu from './components/MenuComponent';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    <div className="App">
-	  	<Navbar dark color="primary">
-        	<div className="container">
-            	<NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          	</div>
-      	</Navbar>
-      	<header className="App-header">
-        	<img src={logo} className="App-logo" alt="logo" />
-        	<p>
-          		Edit <code>src/App.js</code> and save to reload.
-        	</p>
-			<a
-			  className="App-link"
-			  href="https://reactjs.org"
-			  target="_blank"
-			  rel="noopener noreferrer"
-			>
-          	Learn React
-        	</a>
-      	</header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render() {
+	  return (
+		<div className="App">
+			<Navbar dark color="primary">
+				<div className="container">
+					<NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+				</div>	
+			</Navbar>
+		  
+			<Menu dishes={this.state.dishes} />
+
+
+
+
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+				  className="App-link"
+				  href="https://reactjs.org"
+				  target="_blank"
+				  rel="noopener noreferrer"
+				>
+				Learn React
+				</a>
+			</header>
+		</div>
+	  );
+  }
 }
 
 export default App;
